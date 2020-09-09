32197b4cded8a29b6cfa9c32cb7e88722e29d3a9
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-09 16:21:34 +0100

    second solution of pyramid

--- chapter-2-program-structure/chessboard/main.js
+++ chapter-2-program-structure/chessboard/main.js
@@ -13,23 +13,38 @@
 // When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
 
 
-let size = 20;
-let signs = '';
-col1 = '';
-col2 = '';
+let size = 8;
+// FIRST SOLUTION
+// let signs = '';
+// col1 = '';
+// col2 = '';
 
-for (c1 = 0; c1 < size; c1++) {
-    col1 += ' #';
+// for (c1 = 0; c1 < size; c1++) {
+//     col1 += ' #';
+// }
+// for (c2 = 0; c2 < size; c2++) {
+//     col2 += '# ';
+// }
+// for (let l = 0; l < size / 2; l++) {
+//     console.log(col1);
+//     console.log(col2);
+// }
+
+
+
+
+//SECOND SOLUTION
+let board = "";
+for (let l = 0; l < size; l++) {
+    for (let w = 0; w < size; w++) {
+        if ((l + w) % 2 == 0) {
+            board += " ";
+        }
+        else {
+            board += "#";
+        }
+    }
+    board += "\n"
 }
-for (c2 = 0; c2 < size; c2++) {
-    col2 += '# ';
-}
-for (let l = 0; l < size / 2; l++) {
-    console.log(col1);
-    console.log(col2);
-}
 
-
-
-
-
+console.log(board)
