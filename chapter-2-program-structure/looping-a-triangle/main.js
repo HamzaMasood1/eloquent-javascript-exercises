6359caa723a4039be4acb6d05331be257b5b2aca
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-09 15:17:31 +0100

    finished fizzbuzz

--- chapter-2-program-structure/looping-a-triangle/main.js
+++ chapter-2-program-structure/looping-a-triangle/main.js
@@ -1,3 +1,13 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
 hash = ''
 for (let i = 1; i <= 7; i++) {
     console.log(hash += '#')
