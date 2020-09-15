d0b4d46df4297612b864c448ff40a1545424c7ff
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-15 09:10:25 +0100

    finished reverse array in place

--- chapter-4-data-structures/reversing-array.js
+++ chapter-4-data-structures/reversing-array.js
@@ -10,12 +10,17 @@ function reverseArray(array) {
 
 //reverse array in place
 function reverseArrayInPlace(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i])
+    swap = 0;
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+        old = array[i]
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = old;
+        console.log("i=" + i)
     }
+    return array;
 }
 
 //printing
 //console.log(reverseArray(['a', 'b', 'c']));
-console.log(reverseArrayInPlace([1, 2, 3]));
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8]));
 
