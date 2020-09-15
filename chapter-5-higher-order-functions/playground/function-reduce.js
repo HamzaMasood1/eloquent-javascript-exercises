a91b64b584dd6cf8b22d6c35aeae4152f95efb20
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-15 18:40:36 +0100

    reduce function for script data

--- chapter-5-higher-order-functions/playground/function-reduce.js
+++ chapter-5-higher-order-functions/playground/function-reduce.js
@@ -1,3 +1,4 @@
+require("./script-data-set/scripts")
 function reduce(array, combine, start) {
     let current = start;
     for (let element of array) {
@@ -7,3 +8,14 @@ function reduce(array, combine, start) {
 }
 
 console.log(reduce(["1", "2", "3"], (a, b) => a + b, ""));
+
+//using the reduce array to find the script with the highest character count
+
+function characterCount(script) {
+    return script.ranges.reduce((count, [from, to]) => count + (to - from), 0);
+}
+
+console.log(SCRIPTS.reduce((a, b) => {
+    return characterCount(a) < characterCount(b) ? b : a;
+}))
+
