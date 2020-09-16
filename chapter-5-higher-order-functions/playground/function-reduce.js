1a2efa1dc407655176a89a87d9f8dc7156ee5508
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-16 13:15:38 +0100

    reduce function final commit

--- chapter-5-higher-order-functions/playground/function-reduce.js
+++ chapter-5-higher-order-functions/playground/function-reduce.js
@@ -17,11 +17,13 @@ function characterCount(script) {
     let funct = function (count, [from, to]) {
         return count + (to - from)
     }
-//count is the start value of 0. You do not have to define count inside the function, it is already defined in the parameter underneath. This time you have to define count as 0 so it is not defined as a 2d array.
+    //count is the start value of 0. You do not have to define count inside the function, it is already defined in the parameter underneath. This time you have to define count as 0 so it is not defined as a 2d array.
     return script.ranges.reduce(funct, 0)
 }
-console.log(characterCount(SCRIPTS[1]))
-// console.log(SCRIPTS.reduce((a, b) => {
-//     return characterCount(a) < characterCount(b) ? b : a;
-// }))
+//console.log(characterCount(SCRIPTS[1]))
+let longestScript = SCRIPTS.reduce((a, b) => {
+    return characterCount(a) < characterCount(b) ? b : a;
+})
+
+console.log("the longest script is " + longestScript.name + " with " + characterCount(longestScript) + " characters")
 
