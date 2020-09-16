b13c069a81a17d5ce51b335c3ac46370df62e3f5
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-16 12:18:04 +0100

    different way of implementing reduce without making a local var

--- chapter-5-higher-order-functions/playground/function-reduce.js
+++ chapter-5-higher-order-functions/playground/function-reduce.js
@@ -1,18 +1,22 @@
 require("./script-data-set/scripts")
 function reduce(array, combine, start) {
-    let current = start;
     for (let element of array) {
-        current = combine(current, element);
+        start = combine(start, element);
     }
-    return current;
+    return start;
 }
 
 console.log(reduce(["1", "2", "3"], (a, b) => a + b, ""));
 
 //using the reduce array to find the script with the highest character count
 
+//SINCE REDUCE() IS A STANDARD ARRAY FUNCTION THE PARAMETERS CHANGE.
 function characterCount(script) {
-    return script.ranges.reduce((count, [from, to]) => count + (to - from), 0);
+    //return script.ranges.reduce((count, [from, to]) => count + (to - from), 0);
+    let funct = function (count, [from, to]) {
+        return count + (to - from)
+    }
+    return script.ranges.reduce(funct, 0)
 }
 
 console.log(SCRIPTS.reduce((a, b) => {
