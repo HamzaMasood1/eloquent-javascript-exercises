24ae52b0a2fc6a99cb71dc2fd1e3d12fc65780b9
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-16 17:58:01 +0100

    Check the ratio between different scripts in a sentence

--- chapter-5-higher-order-functions/playground/what-script.js
+++ chapter-5-higher-order-functions/playground/what-script.js
@@ -1,5 +1,5 @@
 //One use of the data set would be figuring out what script a piece of text is using. Let’s go through a program that does this.
-
+require("./script-data-set/scripts")
 function characterScript(code) {
     for (let script of SCRIPTS) {
         if (script.ranges.some(([from, to]) => {
@@ -10,3 +10,36 @@ function characterScript(code) {
     }
     return null;
 }
+
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({ name, count: 1 });
+        } else {
+            counts[known].count++;
+        }
+    }
+
+    return counts;
+}
+//console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+
+function textScripts(text) {
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.name : "none";
+    }).filter(({ name }) => name != "none");
+
+    let total = scripts.reduce((n, { count }) => n + count, 0);
+    if (total == 0) return "No scripts found";
+
+    return scripts.map(({ name, count }) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+}
+
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+// → 61% Han, 22% Latin, 17% Cyrillic
